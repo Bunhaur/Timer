e3d2694b26b0a4e940e1945f4a51ad4e9c878421
Bunhaur <86873526+Bunhaur@users.noreply.github.com>
2024-07-07 19:55:07 +0400

    Add files via upload

---
+++ ShowTimer.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+
+public class ShowTimer : MonoBehaviour
+{
+    [SerializeField] private Timer _timer;
+    private TextMeshProUGUI _timerText;
+
+    private void Awake()
+    {
+        _timerText = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void OnEnable()
+    {
+        _timer.TimeIsChanged += Show;
+    }
+
+    private void OnDisable()
+    {
+        _timer.TimeIsChanged -= Show;
+    }
+
+    private void Show(int time)
+    {
+        _timerText.text = time.ToString();
+    }
+}
